13fc74cb43162dbd30d24f91655d9631d45f4227
unknown <aruchanism@gmail.com>
2024-10-29 18:33:20 +0500

    added db context

--- ShopApp.Infrastructure/AppDbContext.cs
+++ ShopApp.Infrastructure/AppDbContext.cs
@@ -1,8 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using ShopApp.Domain;
 
 namespace ShopApp.Infrastructure;
 
 public class AppDbContext : DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){  }
+
+    public DbSet<User> Users { get; set; }
+
 }
